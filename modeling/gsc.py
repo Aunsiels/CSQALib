b4af0e9bf06e7a3fab5af4af5bd4a4a114400f0f
Matthias Hasler <matthias@hasler.fr>
2022-08-31 16:09:32 +0200

    123

--- modeling/gsc.py
+++ modeling/gsc.py
@@ -11,6 +11,7 @@ class GraphHardCounter(nn.Module):
         self.scorer = nn.Embedding(num_rels * 3 * 3, 1)
 
     def forward(self, graph, text):
+        device = graph.node_type.device
         nt = graph.node_type.detach().cpu().numpy()
         et = graph.edge_type.detach().cpu().numpy()
         ei = graph.edge_index.T.detach().cpu().numpy()
@@ -18,7 +19,7 @@ class GraphHardCounter(nn.Module):
         edge_enc = torch.tensor(np.array([
             et[i] * 3*3 + nt[ei[i, 0]] * 3 + nt[ei[i, 1]]
             for i in range(et.shape[0])
-        ]))
+        ])).to(device)
         edge_scores = self.scorer(edge_enc)
 
         return edge_scores.sum()
