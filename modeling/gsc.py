39b5d79cb38f4431c3e5ad2a48f9b9ba3ba11541
Matthias Hasler <matthias@hasler.fr>
2022-08-31 16:50:41 +0200

    123

--- modeling/gsc.py
+++ modeling/gsc.py
@@ -10,16 +10,17 @@ class GraphHardCounter(nn.Module):
         # 3 node types
         self.scorer = nn.Embedding(num_rels * 3 * 3, 1)
 
-    def forward(self, graph, text):
-        device = graph.node_type.device
-        nt = graph.node_type.detach().cpu().numpy()
-        et = graph.edge_type.detach().cpu().numpy()
-        ei = graph.edge_index.T.detach().cpu().numpy()
+    def forward(self, graphs, text):
+        def score(graph):
+            device = graph.node_type.device
+            nt = graph.node_type.detach().cpu().numpy()
+            et = graph.edge_type.detach().cpu().numpy()
+            ei = graph.edge_index.T.detach().cpu().numpy()
 
-        edge_enc = torch.tensor(np.array([
-            et[i] * 3*3 + nt[ei[i, 0]] * 3 + nt[ei[i, 1]]
-            for i in range(et.shape[0])
-        ])).to(device)
-        edge_scores = self.scorer(edge_enc)
+            edge_enc = torch.tensor(np.array([
+                et[i] * 3*3 + nt[ei[i, 0]] * 3 + nt[ei[i, 1]]
+                for i in range(et.shape[0])
+            ])).to(device)
+            return self.scorer(edge_enc).sum()
 
-        return edge_scores.sum()
+        return torch.stack([score(graph) for graph in graphs.to_data_list()])
